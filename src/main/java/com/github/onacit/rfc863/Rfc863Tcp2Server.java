f738901ac84abe2463ae8a9fff94ddd54cf5faab
Jin Kwon <onacit@gmail.com>
2025-02-09 19:45:21 +0900

    wip

--- src/main/java/com/github/onacit/rfc863/Rfc863Tcp2Server.java
+++ src/main/java/com/github/onacit/rfc863/Rfc863Tcp2Server.java
@@ -13,33 +13,40 @@ class Rfc863Tcp2Server {
     public static void main(final String... args) throws IOException {
         try (var selector = Selector.open();
              var server = ServerSocketChannel.open()) {
-            try {
-                server.setOption(StandardSocketOptions.SO_REUSEADDR, Boolean.TRUE);
-            } catch (final Exception e) {
-                log.error("failed to set {}", StandardSocketOptions.SO_REUSEADDR, e);
+            {
+                try {
+                    server.setOption(StandardSocketOptions.SO_REUSEADDR, Boolean.TRUE);
+                } catch (final Exception e) {
+                    log.error("failed to set {}", StandardSocketOptions.SO_REUSEADDR, e);
+                }
+                try {
+                    server.setOption(StandardSocketOptions.SO_REUSEPORT, Boolean.TRUE);
+                } catch (final Exception e) {
+                    log.error("failed to set {}", StandardSocketOptions.SO_REUSEPORT, e);
+                }
+                try {
+                    server.socket().setReuseAddress(true);
+                } catch (final Exception e) {
+                    log.error("failed to set reuseAddress", e);
+                }
             }
-            try {
-                server.setOption(StandardSocketOptions.SO_REUSEPORT, Boolean.TRUE);
-            } catch (final Exception e) {
-                log.error("failed to set {}", StandardSocketOptions.SO_REUSEPORT, e);
-            }
-            try {
-                server.socket().setReuseAddress(true);
-            } catch (final Exception e) {
-                log.error("failed to set reuseAddress", e);
+            {
+                server.bind(_Rfc863Constants.SERVER_ENDPOINT_TO_BIND);
+                log.info("bound to {}", server.getLocalAddress());
             }
             server.configureBlocking(false);
-            server.bind(_Rfc863Constants.SERVER_ENDPOINT_TO_BIND);
-            log.info("bound to {}", server.getLocalAddress());
             final var serverKey = server.register(selector, SelectionKey.OP_ACCEPT);
-            _Rfc863Utils.readQuitAndCall(() -> {
-                serverKey.cancel();
-                selector.wakeup();
-                return null;
-            });
+            {
+                _Rfc863Utils.readQuitAndCall(() -> {
+                    serverKey.cancel();
+                    selector.wakeup();
+                    return null;
+                });
+            }
             final var buffer = ByteBuffer.allocate(1);
             while (serverKey.isValid()) {
-                selector.select(0L);
+                final var count = selector.select(0L);
+                assert count >= 0; // why not 1?
                 for (final var i = selector.selectedKeys().iterator(); i.hasNext(); i.remove()) {
                     final var key = i.next();
                     final var channel = key.channel();
@@ -50,6 +57,7 @@ class Rfc863Tcp2Server {
                         client.configureBlocking(false);
                         client.register(selector, SelectionKey.OP_READ);
                     } else if (key.isReadable()) {
+                        assert channel instanceof SocketChannel;
                         final var r = ((ReadableByteChannel) channel).read(buffer.clear());
                         if (r == -1) {
                             key.cancel();
