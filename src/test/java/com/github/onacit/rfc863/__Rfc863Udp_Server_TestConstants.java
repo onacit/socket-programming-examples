a65fa4b76afe153a6738759f3dc9bd517ca97ea8
Jin Kwon <onacit@gmail.com>
2025-04-01 12:10:26 +0900

    wip

---
+++ src/test/java/com/github/onacit/rfc863/__Rfc863Udp_Server_TestConstants.java
@@ -0,0 +1,8 @@
+package com.github.onacit.rfc863;
+
+final class __Rfc863Udp_Server_TestConstants {
+
+    private __Rfc863Udp_Server_TestConstants() {
+        throw new AssertionError("instantiation is not allowed");
+    }
+}
